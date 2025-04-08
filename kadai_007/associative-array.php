77b71790621506b1b5aeecf188b2abaa96308377
honestboy1 <longyixiaoquan74@gmail.com>
2025-04-08 21:42:02 +0900

    kadai_007

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>kadai_007</title>
+</head>
+
+<body>
+  <P>
+    <?php
+
+    $vegitable_data = ['name' => 'onion','price' => '36','weight' => '160'];
+
+    print_r($vegitable_data);
+
+    ?>
+  </p>
+</body>
+
+</html>
